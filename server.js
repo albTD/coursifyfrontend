7b497daace2dffce82c7497858c2d18d0f25d49e
albTD <albinsam8081@gmail.com>
2020-11-21 05:33:26 +0530

    first

---
+++ server.js
@@ -0,0 +1,26 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+
+const app = express();
+
+// app.use('/', express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+
+
+
+//TODO
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
+});
